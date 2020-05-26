f8a05688f4b016f911d5acac828c54196216c05a
chicot1k <58642288+chicot1k@users.noreply.github.com>
2020-05-26 15:25:31 +0300

    Add files via upload

---
+++ LINQ10/LINQ10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ10
+{
+    class Program
+    {
+        public class client : IComparable<client>
+        {
+            public int code, year, month, hours;
+            public client(int c, int y, int m, int h)
+            {
+                this.code = c;
+                this.month = m;
+                this.year = y;
+                this.hours = h;
+            }
+            public int CompareTo(client another)
+            {
+                return this.hours.CompareTo(another.hours);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n, d, y, m, h;
+            var current = new List<client>();
+            Console.WriteLine("Введите количество клиентов");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите Список клиентов(код, год, месяц, часы)");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Клиент " + (i + 1));
+                d = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                m = Convert.ToInt32(Console.ReadLine());
+                h = Convert.ToInt32(Console.ReadLine());
+                current.Add(new client(d, y, m, h));
+            }
+            current.Reverse();
+            var another = current;
+            client min = another.Min();
+            Console.Write("Минимальная продолжительность занятий в часах: " + min.hours + " год: " + min.year + " месяц: " + min.month);
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ LINQ8/LINQ8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива A:\t");
+            int elementcount = int.Parse(Console.ReadLine());
+            int[] myArray = new int[elementcount];
+            Console.Write("Введите элементы  массива:\t");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Введите количество элементов массива B:\t");
+            int elementcount1 = int.Parse(Console.ReadLine());
+            int[] myArray1 = new int[elementcount1];
+            Console.Write("Введите элементы  массива:\t");
+            for (int i = 0; i < myArray1.Length; i++)
+            {
+                myArray1[i] = int.Parse(Console.ReadLine());
+            }
+            var result = myArray.Select(s => new { f = s, last = s % 10 });
+            var result1 = myArray1.Select(s => new { f1 = s, last = s % 10 });
+            var result3 = from item in result
+                          join item2 in result1
+                               on item.last equals item2.last
+                          select (item.f + "-" + item2.f1);
+            Console.WriteLine("Отсортированная последовательность: ");
+            foreach (var i in result3)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
